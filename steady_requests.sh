6723676ec85a8b239d7ba401215878d4f25a6982
Mourjo Sen <mourjo@helpshift.com>
2020-08-11 07:11:57 +0530

    Delete unused scripts

--- steady_requests.sh
+++
@@ -1,7 +0,0 @@
-for i in $(seq 1 10000); do
-
-curl -L -s "http://localhost:3100/slow" > /dev/null &
-curl -L -s "http://localhost:3200/slow" > /dev/null &
-echo $(date) ": Request " $i;
-sleep ${SLEEP:=3}
-done;
