0c5c38abc17de3fb81830af4513b291c4be99068
Mourjo Sen <hello@mourjo.me>
2024-10-01 15:04:26 +0200

    Update description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject procrustes "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Demo of how loadshedding can benefit your web server"
+  :url "https://mourjo.me"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
